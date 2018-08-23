c9289ba50134dec8dfefa311fa2de30d61dd0bd8
David Worms <david@adaltas.com>
2018-08-23 23:50:28 +0200

    home: rephrase title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>A Node.js client to HBase using REST</h1>
+    <h1>HBase client for Node.js using REST</h1>
     <h2>Introduction</h2>
     <p>Node HBase is a Node.JS client for the Apache HBase database. It use the Rest API (Stargate) to communicate with HBase. Currently, all the API is implemented and the data exchange format is JSON (but protocol buffer could follow).</p>
     <p>Apache HBase is part of the Hadoop ecosystem. It describes itself as the Hadoop database optimized for random, realtime read/write access to big data. It is an open-source, distributed, versioned, column-oriented store modeled after Google Bigtable.</p>
