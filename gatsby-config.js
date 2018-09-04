b2b070d86906f84f8b542d2e27eb6f05286e555a
David Worms <david@adaltas.com>
2018-09-04 23:58:40 +0200

    manifest: add hbase logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,10 +72,10 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#3E863D',
+        theme_color: '#3E863D',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo.png',
       },
     },
     'gatsby-plugin-offline',
